588870621734d5f450299fb5bc0712b7f20ab162
seos1218 <sebasoapina1218@gmail.com>
2023-06-06 14:17:17 -0500

    Se organizó la parte visual del trabajo y se implementó un buscador que por el momento no toma los valores de la tabla

--- src/app/facultades/service/facultades.service.ts
+++ src/app/facultades/service/facultades.service.ts
@@ -9,13 +9,13 @@ import {Facultad} from "../model/facultad";
 export class FacultadesService {
 
   //Se define la url del servicio REST
-  private urlEndPoint: string = 'http://localhost:8081/facultad-service/facultad';
+ private urlEndPoint: string = 'http://localhost:8081/facultad-service/facultad';
 
   private baseUrl: string = "http://localhost:8081/facultad-service/listar"
 
   //Metodo para crear un pais con HttpClient por el metodo POST
   save(facultad: any): Observable<Facultad> { //Se retorna un objeto observable que espera a que este disponible el objeto Pais
-    return this.httpClient.post<Facultad>(this.urlEndPoint, facultad).pipe( //Se realiza la peticion POST al servicio REST
+    return this.httpClient.post<Facultad>(this.baseUrl, facultad).pipe( //Se realiza la peticion POST al servicio REST
       catchError(() => throwError(() => new Error('Error al crear la facultad'))) //Se captura el error en caso de que exista
     );
   }
